a6fef0272072efb9f7505a490d438c47aa548f66
dharun11 <dharunashok113@gmail.com>
2024-12-17 18:27:14 +0530

    updated inference

--- src/components/llm_trainer.py
+++ src/components/llm_trainer.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, BitsAndBytesConfig
